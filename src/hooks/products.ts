f869fb96d7006a345edc8de1b668a5d1784ed6e4
solidados <geekbrains.pk@internet.ru>
2022-11-25 16:47:44 +0400

    feat: move all app logic to hooks

---
+++ src/hooks/products.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { IProduct } from '../models';
+import axios, {AxiosError} from 'axios';
+
+export function useProducts() {
+    const [products, setProducts] = useState<IProduct[]>([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+    // const AxiosError = require('axios-error');
+
+    async function fetchProducts() {
+        try {
+            setError('')
+            setLoading(true) /* перед началом загрузки значение = true */
+            const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=7')
+            setProducts(response.data)
+            setLoading(false) /* после окончания загрузки значение = false */
+        } catch (e: unknown) {
+            const error = e as AxiosError
+            setLoading(false)
+            setError(error.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    return { products, error, loading }
+}
