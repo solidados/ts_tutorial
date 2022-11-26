b142d64033fa307c2d7c9551bad55d79f19337ee
solidados <geekbrains.pk@internet.ru>
2022-11-26 22:14:46 +0400

    feat: unify & add ModalContext open/close

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import { Loader } from "./components/Loader";
 import { ErrorMessage } from './components/ErrorMessage';
 import { Modal } from "./components/Modal";
 import { CreateProduct } from './components/CreateProduct';
-import { useState } from 'react';
+import { useContext } from 'react';
 import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 
 function App() {
   const { loading, error, products, addProduct } = useProducts()
-  const [modal, setModal] = useState(false)
+  // const [modal, setModal] = useState(false)
+  const { modal, open: openModal, close: closeModal } = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    closeModal()
     addProduct(product)
   }
 
@@ -23,13 +25,13 @@ function App() {
       {error && <ErrorMessage error={error} />}
       {products.map(product => <Product product={product} key={product.id} />)}
 
-      {modal && <Modal title='Create new product' onClose={() => setModal(false) }>
+      {modal && <Modal title='Create new product' onClose={closeModal}>
         <CreateProduct onCreate={createHandler} />
       </Modal>}
 
       <button
-      className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-1xl px-4 py-2'
-      onClick={() => setModal(true)}
+        className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-1xl px-4 py-2'
+        onClick={openModal}
       >+</button>
     </div>
   )
